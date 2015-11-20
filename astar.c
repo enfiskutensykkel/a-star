35154cd8245c9c3061c0cab3082b08ee30401b1f
Jonas Markussen <jonassm@ifi.uio.no>
2015-11-20 12:01:27 +0100

    Optimized my algorithm

--- astar.c
+++ astar.c
@@ -2,40 +2,114 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
-#include <math.h>
-#include <float.h>
-
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 
-char* map = NULL;
-uint32_t width = 0, height = 0;
 
-uint8_t* map_exits = NULL;
-uint8_t* closed_list = NULL;
-
-double* distances = NULL;
-uint32_t* path = NULL;
-uint32_t* heap = NULL;
-uint32_t heap_size = 0;
-
-int is_marked(uint8_t* bitmap, uint32_t idx)
+int read_map(FILE* file, char* map, uint64_t* objects, uint32_t width, uint32_t height)
 {
-    return !!(bitmap[idx >> 3] & (1 << (idx & 7)));
+    for (int i = 0; i < 256; ++i)
+    {
+        objects[i] = 0;
+    }
+
+    for (uint32_t i = 0, n = width * height; i < n; ++i)
+    {
+        int c;
+
+        while ((c = fgetc(file)) != EOF && (c < 0x20 || c > 0x7e));
+
+        if (c == EOF)
+        {
+            return 0;
+        }
+
+        map[i] = c & 255;
+        ++objects[c & 255];
+    }
+
+    return 1;
 }
 
-void mark(uint8_t* bitmap, uint32_t idx)
+
+
+int read_metadata(FILE* file, uint32_t* width, uint32_t* height)
 {
-    bitmap[idx >> 3] |= 1 << (idx & 7);
+    char line[2048];
+    char* pos;
+    
+    int status = 0;
+
+    for (int i = 0; i < 4 && !feof(file); ++i)
+    {
+        fgets(line, sizeof(line), file);
+
+        for (size_t j = 0; j < sizeof(line); ++j)
+        {
+            if (line[j] < 0x20 || line[j] > 0x7e)
+            {
+                line[j] = '\0';
+                break;
+            }
+        }
+
+        if (strncmp("width ", line, 6) == 0)
+        {
+            pos = NULL;
+            *width = strtoul(&line[6], &pos, 0);
+            if (pos == NULL || *pos != '\0')
+            {
+                return 0;
+            }
+            status |= 1;
+        }
+        else if (strncmp("height ", line, 7) == 0)
+        {
+            pos = NULL;
+            *height = strtoul(&line[7], &pos, 0);
+            if (pos == NULL || *pos != '\0')
+            {
+                return 0;
+            }
+            status |= 2;
+        }
+    }
+
+    return !!(status & 3);
 }
 
-void heap_insert(uint32_t point)
-{
-    uint32_t hole = ++heap_size;
 
-    for (; hole > 1 && distances[point] < distances[heap[hole >> 1]]; hole >>= 1)
+
+void print_path(char* map, uint32_t width, uint32_t height, uint32_t* rev_path, uint32_t point)
+{
+    if (rev_path[point] != point && rev_path[point] < width * height)
+    {
+        print_path(map, width, height, rev_path, rev_path[point]);
+    }
+    fprintf(stdout, "(%u,%u)\n", point % width, point / height);
+    map[(point / height) * width + (point % width)] = 'x';
+}
+
+
+
+void print_map(char* map, uint32_t width, uint32_t height)
+{
+    for (uint32_t y = 0; y < width; ++y)
+    {
+        for (uint32_t x = 0; x < width; ++x)
+        {
+            fprintf(stderr, "%c", map[y * width + x]);
+        }
+        fprintf(stderr, "\n");
+    }
+}
+
+
+
+void heap_insert(uint32_t* heap, uint32_t* heap_size, double* fcosts, uint32_t point)
+{
+    uint32_t hole;
+
+    for (hole = ++*heap_size; hole > 1 && fcosts[point] < fcosts[heap[hole >> 1]]; hole >>= 1)
     {
         heap[hole] = heap[hole >> 1];
     }
@@ -43,24 +117,25 @@ void heap_insert(uint32_t point)
     heap[hole] = point;
 }
 
-uint32_t heap_remove()
+
+
+uint32_t heap_remove(uint32_t* heap, uint32_t* heap_size, double* fcosts)
 {
-    uint32_t remove = heap[1];
+    uint32_t removed_point = heap[1];
 
-    uint32_t node = heap[1] = heap[heap_size--];
+    uint32_t point = heap[1] = heap[(*heap_size)--];
+    uint32_t hole, child;
 
-    uint32_t hole = 1, child;
-
-    for (; (hole << 1) <= heap_size; hole = child)
+    for (hole = 1; (hole << 1) <= *heap_size; hole = child)
     {
         child = hole << 1;
 
-        if (child != heap_size && distances[heap[child + 1]] < distances[heap[child]])
+        if (child != *heap_size && fcosts[heap[child + 1]] < fcosts[heap[child]])
         {
             ++child;
         }
 
-        if (distances[heap[child]] < distances[node])
+        if (fcosts[heap[child]] < fcosts[point])
         {
             heap[hole] = heap[child];
         }
@@ -70,269 +145,241 @@ uint32_t heap_remove()
         }
     }
 
-    heap[hole] = node;
-    return remove;
+    heap[hole] = point;
+
+    return removed_point;
 }
 
 
-double manhatten(int64_t sx, int64_t sy, int64_t dx, int64_t dy) 
+
+int is_marked(uint8_t* bitmap, uint32_t point)
 {
-    int64_t tx = llabs(sx - dx);
-    int64_t ty = llabs(sy - dy);
+    return !!(bitmap[point >> 3] & (1 << (point & 7)));
+}
+
+
+
+void mark(uint8_t* bitmap, uint32_t point)
+{
+    bitmap[point >> 3] |= 1 << (point & 7);
+}
+
+
+
+double manhatten(uint32_t ux, uint32_t uy, uint32_t vx, uint32_t vy)
+{
+    uint64_t tx, ty;
+
+    tx = (ux >= vx) ? ux - vx : vx - ux;
+    ty = (uy >= vy) ? uy - vy : vy - uy;
 
     return 2.0 * (tx + ty);
 }
 
-double chebyshev(int64_t sx, int64_t sy, int64_t dx, int64_t dy) 
+
+
+uint32_t search(char* map, uint32_t width, uint32_t height, double* cost_lut, uint32_t* rev_path, uint32_t start, uint32_t target)
 {
-    double D = 14.0;
+    // Allocate resources
+    double* f_costs = (double*) malloc(sizeof(double) * width * height);
+    uint32_t* g_costs = (uint32_t*) malloc(sizeof(uint32_t) * width * height);
+    uint8_t* closed_list = (uint8_t*) malloc(width * height);
 
-    int64_t tx = llabs(sx - dx);
-    int64_t ty = llabs(sy - dy);
-    return D * (tx + ty) + (D * 2.0 - 2.0 * D) * MIN(tx, ty);
-} 
-
-uint32_t find_closest_exit(uint32_t point, uint32_t e_x, uint32_t e_y)
-{
-    distances[point] = 0;
-    path[point] = point;
-
-    heap_insert(point);
-
-    while (heap_size > 0)
+    // Initialize closed list and cost table and reverse path
+    for (uint32_t i = 0, n = width * height; i < n; ++i)
     {
-        uint32_t u = heap_remove();
-        int64_t x = u % width;
-        int64_t y = u / height;
-
-        //if (is_marked(map_exits, u))
-        if (x == e_x && y == e_y)
-        {
-            // found a way out
-            return u;
-        }
-
-        // remove u from open list and insert into closed list
-        mark(closed_list, u);
-
-        for (int64_t j = MAX(y - 1, 0); j <= MIN(y + 1, height - 1); ++j)
-        {
-            for (int64_t i = MAX(x - 1, 0); i <= MIN(x + 1, width - 1); ++i)
-            {
-                uint32_t v = j * width + i;
-
-                if (u != v && map[v] == '.' && !is_marked(closed_list, v))
-                {
-                    double alt = distances[u] + 1 + manhatten(x, y, e_x, e_y); //chebyshev(x, y, e_x, e_y);
-
-                    if (alt < distances[v])
-                    {
-                        path[v] = u;
-                        distances[v] = alt;
-
-                        heap_insert(v);
-                    }
-                }
-            }
-        }
+        closed_list[i >> 3] = 0;
+        f_costs[i] = width * height; // infinity
+        g_costs[i] = width * height;
+        rev_path[i] = i;
     }
 
-    return width * height;
-}
+    // Initialize open list
+    uint32_t* open_list = (uint32_t*) malloc(sizeof(uint32_t) * width * height);
+    uint32_t open_list_size = 0;
 
-int parse_map_metadata(uint32_t* width, uint32_t* height, FILE* fp)
-{
-    char* line = (char*) malloc(2048);
-    char* pos;
-    int c, status;
-    size_t i;
+    uint32_t found = width * height;
 
-    i = status = 0;
-    while ((c = fgetc(fp)) != EOF)
+    // Initalize with start node
+    f_costs[start] = 0.0;
+    g_costs[start] = 0;
+    heap_insert(open_list, &open_list_size, f_costs, start);
+
+    while (open_list_size > 0)
     {
-        if (i >= 2048 || status == 3)
+        // remove from open list the point u with smallest f_cost
+        uint32_t u = heap_remove(open_list, &open_list_size, f_costs);
+        uint32_t ux = u % width;
+        uint32_t uy = u / height;
+
+        if (u == target)
         {
+            found = u;
             break;
         }
-        else if (c == '\n')
-        {
-            line[i] = '\0';
 
-            if (i >= 7 && !strncmp("height ", line, 7))
+        // insert u into closed list
+        mark(closed_list, u);
+
+        // do some boundary checking magic
+        uint32_t vx_lo = ux > 0 ? ux - 1 : 0;
+        uint32_t vx_hi = ux < width - 1 ? ux + 1 : width - 1;
+        uint32_t vy_lo = uy > 0 ? uy - 1: 0;
+        uint32_t vy_hi = uy < height - 1 ? uy + 1 : height - 1;
+
+        // for neighbour v of u:
+        for (uint32_t vy = vy_lo; vy <= vy_hi; ++vy)
+        {
+            for (uint32_t vx = vx_lo; vx <= vx_hi; ++vx)
             {
-                pos = NULL;
-                *height = strtoul(&line[7], &pos, 0);
-                if (pos == NULL || *pos != '\0' || *height <= 3)
+                uint32_t v = vy * width + vx;
+
+                // if v is not marked
+                if (u == v && !is_marked(closed_list, v))
                 {
-                    break;
+                    continue;
                 }
-                status |= 2;
-            }
-            else if (i >= 6 && !strncmp("width ", line, 6))
-            {
-                pos = NULL;
-                *width = strtoul(&line[6], &pos, 0);
-                if (pos == NULL || *pos != '\0' || *width <= 3)
+
+                // calculate tentative g score
+                uint32_t alt = g_costs[u] + 1;
+
+                // use u to reach v if path to v is shorter through u
+                if (alt < g_costs[v])
                 {
-                    break;
+                    rev_path[v] = u;
+                    g_costs[v] = alt;
+                    f_costs[v] = alt + cost_lut[map[v]] + manhatten(ux, uy, vx, vy);
+
+                    heap_insert(open_list, &open_list_size, f_costs, v);
                 }
-                status |= 1;
             }
-
-            i = 0; 
-        }
-        else
-        {
-            line[i++] = c & 255;
         }
     }
 
-    free(line);
-    return status;
+    free(open_list);
+    free(closed_list);
+    free(f_costs);
+    free(g_costs);
+    return found;
 }
 
-int read_map(FILE* file)
-{
-    // Read map
-    for (uint32_t i = 0; i < width * height; ++i)
-    {
-        int c;
-        
-        while ((c = fgetc(file)) != EOF && c != '.' && c != '@');
 
-        if (c == EOF)
-        {
-            return 0;
-        }
-
-        map[i] = c & 255;
-    }
-
-    return 1;
-}
-
-void print_path(uint32_t node)
-{
-    if (path[node] != width * height && path[node] != node)
-    {
-        print_path(path[node]);
-    }
-    fprintf(stderr, "(%u,%u)\n", node % width, node / width);
-    map[(node / width) * height + node % width] = 'x';
-}
-
-int read_int(uint32_t* i, char* str)
-{
-    char* pos = NULL;
-    *i = strtoul(str, &pos, 0);
-
-    if (pos == NULL || *pos != '\0')
-    {
-        return 0;
-    }
-
-    return 1;
-}
 
 int main(int argc, char** argv)
 {
-    if (argc != 6)
+    if (argc < 6)
     {
-        fprintf(stderr, "Usage: %s <map file> <start x> <start y> <end x> <end y>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <start-x> <start-y> <target-x> <target-y> <map-file>\n", argv[0]);
         return 1;
     }
 
-    FILE* file = fopen(argv[1], "r");
+    // Parse coordinates
+    long coordinates[4] = {-1, -1, -1, -1};
+
+    for (int i = 1; i < argc && i < 5; ++i)
+    {
+        char* pos = NULL;
+        coordinates[i - 1] = strtol(argv[i], &pos, 0);
+
+        if (pos == NULL || *pos != '\0' || coordinates[i-1] < 0)
+        {
+            fprintf(stderr, "'%s' is not a valid map coordinate\n", argv[i]);
+            return 1;
+        }
+    }
+
+    // Parse map file
+    FILE* file = fopen(argv[5], "r");
     if (file == NULL)
     {
-        fprintf(stderr, "%s is not a valid map file\n", argv[1]);
-        return 2;
+        fprintf(stderr, "'%s' is not a valid map\n", argv[5]);
+        return 1;
     }
 
-    if (parse_map_metadata(&width, &height, file) != 3)
+    // Extract map metadata (width and height) from file
+    uint32_t width, height;
+    if (!read_metadata(file, &width, &height))
     {
         fclose(file);
-        fprintf(stderr, "%s is not a valid map file\n", argv[1]);
-        return 2;
+        fprintf(stderr, "'%s' is not a valid map\n", argv[5]);
+        return 1;
     }
 
-    map = (char*) malloc(width * height);
-    if (map == NULL)
+    // Check that given coordinates are legal
+    if (coordinates[0] >= width || coordinates[1] >= height)
     {
         fclose(file);
-        fprintf(stderr, "Out of resources\n");
-        return 3;
+        fprintf(stderr, "(%ld,%ld) is not a point in the map!\n", coordinates[0], coordinates[1]);
+        return 1;
+    }
+    else if (coordinates[0] >= width || coordinates[1] >= height)
+    {
+        fclose(file);
+        fprintf(stderr, "(%ld,%ld) is not a point in the map!\n", coordinates[0], coordinates[1]);
+        return 1;
     }
 
-    if (read_map(file) != 1)
+    char* map = (char*) malloc(width * height);
+    uint64_t objects[256];
+
+    // Parse map file
+    if (!read_map(file, map, objects, width, height))
     {
-        fclose(file);
         free(map);
-        fprintf(stderr, "%s is not a valid map file\n", argv[1]);
-        return 2;
+        fclose(file);
+        fprintf(stderr, "Couldn't parse map '%s'\n", argv[5]);
+        return 1;
     }
 
     fclose(file);
 
-    uint32_t x, y, end_x, end_y;
+    uint32_t sx, sy, tx, ty;
+    sx = (uint32_t) coordinates[0];
+    sy = (uint32_t) coordinates[1];
+    tx = (uint32_t) coordinates[2];
+    ty = (uint32_t) coordinates[3];
+
+    // Print some info
+//    fprintf(stderr, "Map            : %s (%u x %u)\n", argv[5], width, height);
+//    fprintf(stderr, "Objects in map :");
+//    for (int i = 0; i < 256; ++i)
+//    {
+//        if (objects[i] > 0)
+//        {
+//            fprintf(stderr, " %c", i);
+//        }
+//    }
+//    fprintf(stderr, "\n");
+//    fprintf(stderr, "Starting coords: (%u,%u)\n", sx, sy);
+//    fprintf(stderr, "Target coords  : (%u,%u)\n", tx, ty);
+
     
-    if (!read_int(&x, argv[2]) || !read_int(&y, argv[3]) || !read_int(&end_x, argv[4]) || !read_int(&end_y, argv[5]))
+    // Make a cost look-up table
+    double cost_lut[256];
+    cost_lut['@'] = (width * height) * 2.0; // set cost of walls to infinity
+    cost_lut['.'] = 1.0;
+
+    // Make routing table
+    uint32_t* reverse_path = (uint32_t*) malloc(sizeof(uint32_t) * width * height);
+
+    uint32_t s = sy * width + sx;
+    uint32_t t = ty * width + tx;
+
+    uint32_t exit_point = search(map, width, height, cost_lut, reverse_path, s, t);
+
+    if (exit_point == t)
     {
-        fprintf(stderr, "invalid coordinates\n");
-        return 1;
-    }
+        print_path(map, width, height, reverse_path, exit_point);
 
-    fprintf(stderr, "Width: %u\nHeight: %u\nStart: (%u, %u)\nEnd: (%u, %u)\n", width, height, x, y, end_x, end_y);
-
-    
-    // Find exit points
-    map_exits = (uint8_t*) malloc((width * height) >> 3);
-    memset(map_exits, 0, (width * height) >> 3);
-    mark(map_exits, end_y * width + end_x);
-
-    // Set distance to every node = INFINITY
-    distances = (double*) malloc(sizeof(double) * (width * height));
-    path = (uint32_t*) malloc(sizeof(uint32_t) * (width * height));
-    for (uint32_t i = 0; i < width * height; ++i)
-    {
-        distances[i] = DBL_MAX;
-        path[i] = width * height;
-    }
-
-
-    // Create heap and closed list for A* search
-    heap = (uint32_t*) malloc(sizeof(uint32_t) * (width * height));
-    heap_size = 0;
-
-    closed_list = (uint8_t*) malloc((width * height) >> 3);
-    memset(closed_list, 0, width * height);
-
-    uint32_t exit_node = find_closest_exit(y * width + x, end_x, end_y);
-    if (exit_node < width * height)
-    {
-        fprintf(stderr, "Exit found: (%u,%u)\n", exit_node % width, exit_node / width);
-
-        print_path(exit_node);
-
-        for (uint32_t i = 0; i < width * height; ++i)
-        {
-            if (i % width == 0 && i > 0)
-            {
-                printf("\n");
-            }
-            printf("%c", map[i]);
-        }
-        printf("\n");
+        print_map(map, width, height);
     }
     else
     {
-        fprintf(stderr, "No exit found\n");
+        printf("No path found\n");
     }
 
-    free(distances);
-    free(heap);
-    free(closed_list);
-    free(map_exits);
+    free(reverse_path);
     free(map);
+
     return 0;
 }
